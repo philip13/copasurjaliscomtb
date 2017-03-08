94c0d9ae69870c05f2e34e3d0012c5dbb079b9fa
Felipe Cuevas <cuevas_castillo@hotmail.com>
2017-03-08 00:11:56 -0600

    method to translate mounth to spanish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_month_to_es_by_num num_month
+    months_es ||= [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    months_es[num_month]
+  end
 end
